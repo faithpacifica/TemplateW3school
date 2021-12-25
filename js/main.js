72ee2757d8a00ed39a38c04a535ecad8cf365182
Dildora Muslimova <85798640+faithpacifica@users.noreply.github.com>
2021-12-25 12:01:39 +0500

    JS code revised

--- js/main.js
+++ js/main.js
@@ -16,36 +16,34 @@ const elInputEmail = document.getElementById("email");
 const elInputMessage = document.getElementById("msg");
 const elSubmitButton = document.getElementById("submitButton");
 
-// let el1IsValid = false;
-// let el2IsValid = false;
-// let el3IsValid = false;
+let el1IsValid = false;
+let el2IsValid = false;
+let el3IsValid = false;
 
 elInputName.addEventListener("change", function (e) {
-  console.log(e.target.value);
-
-//   if (e.target.value == "") elInputName.style.borderColor = "red";
-//   else elInputName.style.borderColor = "green";
-
+  if(e.target.value.length > 0 ) el1IsValid = true;
   CheckButton();
 });
 
 elInputEmail.addEventListener("change", function (e) {
-  // if (e.target.value != "") el2IsValid = true;
+  if(e.target.value.length > 0 ) el2IsValid = true;
   CheckButton();
-  console.log(e.target.value);
 });
 
 elInputMessage.addEventListener("change", function (e) {
-  console.log(e.target.value);
-  // if (e.target.value != "") el3IsValid = true;
+ if( e.target.value.length > 0 ) el3IsValid = true;
   CheckButton();
 });
 
 const CheckButton = () => {
   elSubmitButton.disabled =
-    elInputName.value.length > 0 &&
-    elInputEmail.value.length > 0 &&
-    elInputMessage.value.length > 0
+  el1IsValid && el3IsValid && el2IsValid
       ? false
       : true;
 };
+
+elSubmitButton.addEventListener('click', () =>{
+ console.log(elInputName.value);
+  console.log(elInputEmail.value);
+  console.log(elInputMessage.value);
+})
